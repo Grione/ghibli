ae2b7c7a89bd60b396bfdee9a814b30940e52ece
Grione <greeb.spb@gmail.com>
2021-08-22 19:16:49 +0300

    add js logic

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const app = document.getElementById('root');
+const logo = document.createElement('IMG');
+
+logo.src = 'logo.png';
+
+const container = document.createElement('DIV');
+container.setAttribute('class', 'container');
+
+app.prepend(logo);
+app.append(container);
+
 fetch('https://ghibliapi.herokuapp.com/films')
 	.then((response) => {
 		console.log(response);
@@ -5,9 +16,24 @@ fetch('https://ghibliapi.herokuapp.com/films')
 	})
 	.then((data) => {
 		data.forEach((movie) => {
-			console.log(movie.title);
+			const card = document.createElement('DIV');
+			card.setAttribute('class', 'card');
+
+			const h1 = document.createElement('H1');
+			h1.textContent = movie.title;
+
+			const p = document.createElement('P');
+			movie.description = movie.description.substring(0, 300);
+			p.textContent = `${movie.description}...`;
+
+			container.append(card);
+			card.append(h1);
+			card.append(p);
 		});
 	})
 	.catch((err) => {
-		console.log(response.status);
+		const errorMessage = document.createElement('marquee');
+		errorMessage.textContent = `Oops, it's not working! See in console.`;
+		app.append(errorMessage);
+		console.log(err);
 	});
